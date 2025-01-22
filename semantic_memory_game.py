ac4e75ca607f2ac418f1a49bffad17614a567e2d
rikifunt <rikifunt@gmail.com>
2025-01-22 16:37:49 +0100

    finish new state implementation and tabular Q-learning

--- semantic_memory_game.py
+++ semantic_memory_game.py
@@ -1,22 +1,29 @@
-from dataclasses import dataclass
-from enum import Enum
+from dataclasses import dataclass, field
+from enum import IntEnum
 from functools import reduce
 from logging import warning
 from typing import Iterable, Iterator, NamedTuple
 
 import numpy as np
 import pygame
+import gymnasium as gym
+from gymnasium import spaces
+
 
 
 @dataclass
-class SemanticMemoryGame:
-    class CardState(Enum):
+class MemoryGame:
+    class CardState(IntEnum):
         UNSEEN = 0 # card was never seen
         FACE_DOWN = 1 # card was seen and is face down
         FACE_UP = 2 # card was seen and is face up
         SOLVED = 3 # card is solved (face up, matched with the correct card)
 
     oracle: np.ndarray[int] # for each card, the card it matches with
+
+    @property
+    def n_cards(self) -> int:
+        return len(self.oracle)
 
     def __post_init__(self):
         if len(self.oracle.shape) != 1:
@@ -25,35 +32,253 @@ class SemanticMemoryGame:
             raise ValueError("Each card must have a unique match")
 
     def reset(self, seed=None) -> np.ndarray[int]:
-        return np.full(len(self.oracle), SemanticMemoryGame.CardState.UNSEEN)
+        return np.full(self.n_cards, MemoryGame.CardState.UNSEEN)
 
     def step(self, state: np.ndarray[int], action: int) -> np.ndarray[int] | None:
-        next_state = state.copy()
+        if (state == MemoryGame.CardState.SOLVED).all():
+            # Game is over (all cards are solved)
+            # print(f'[Game] All cards are solved')
+            return None
 
-        if state[action] == SemanticMemoryGame.CardState.UNSEEN:
-            # Choose a random unseen card
-            card = np.random.choice(np.where(state == SemanticMemoryGame.CardState.UNSEEN)[0])
-            next_state[card] = SemanticMemoryGame.CardState.FACE_UP
-            return next_state
-
-        if state[action] == SemanticMemoryGame.CardState.FACE_DOWN:
-            # Turn card face up
-            next_state[action] = SemanticMemoryGame.CardState.FACE_UP
-            # If there are two face up cards, check if they match
-            face_up = np.where(next_state == SemanticMemoryGame.CardState.FACE_UP)[0]
-            assert len(face_up) <= 2
-            if len(face_up) == 2:
-                if self.oracle[face_up[0]] == face_up[1]:
-                    next_state[face_up] = SemanticMemoryGame.CardState.SOLVED
-                else:
-                    next_state[face_up] = SemanticMemoryGame.CardState.FACE_DOWN
-            return next_state
-
-        if state[action] == SemanticMemoryGame.CardState.FACE_UP or state[action] == SemanticMemoryGame.CardState.SOLVED:
+        if state[action] == MemoryGame.CardState.FACE_UP or state[action] == MemoryGame.CardState.SOLVED:
             # Do nothing (invalid action)
-            return next_state
+            # print(f'[Game] Card {action} is already face up or solved')
+            return state.copy()
+
+        # Action is valid: turn card face up (if unseen card chosen, randomize
+        # the choice)
+
+        if state[action] == MemoryGame.CardState.UNSEEN:
+            # Choose a random unseen card
+            action = np.random.choice(np.where(state == MemoryGame.CardState.UNSEEN)[0])
+            # print(f'[Game] choosing random unseen card: {action}')
+
+        return self.turn_face_up(state, action)
+
+    def turn_face_up(self, state: np.ndarray[int], card: int) -> np.ndarray[int]:
+        next_state = state.copy()
+        # print(f'[Game] Turning card {card} face up')
+        # Turn card face up
+        next_state[card] = MemoryGame.CardState.FACE_UP
+        # If there are two face up cards, check if they match
+        face_up = np.where(next_state == MemoryGame.CardState.FACE_UP)[0]
+        assert len(face_up) <= 2
+        if len(face_up) == 2:
+            if self.oracle[face_up[0]] == face_up[1]:
+                # print(f'[Game] Matched cards {face_up[0]} and {face_up[1]} :)')
+                next_state[face_up] = MemoryGame.CardState.SOLVED
+            else:
+                # print(f'[Game] Cards {face_up[0]} and {face_up[1]} do not match :(')
+                next_state[face_up] = MemoryGame.CardState.FACE_DOWN
+        return next_state
 
     def have_new_match(self, state: np.ndarray[int], next_state: np.ndarray[int]) -> bool:
         # number of solved cards increased
-        return np.sum(next_state == SemanticMemoryGame.CardState.SOLVED) > np.sum(state == SemanticMemoryGame.CardState.SOLVED)
+        return np.sum(next_state == MemoryGame.CardState.SOLVED) > np.sum(state == MemoryGame.CardState.SOLVED)
 
+
+@dataclass
+class InteractiveMatch:
+    game: MemoryGame
+    width: int
+    height: int
+    cards: tuple[str, ...]
+    categories: tuple[str, ...]
+    rows: int = field(init=False)
+    cols: int = field(init=False)
+    
+    def __post_init__(self):
+        self.rows, self.cols = self.grid_dimensions(self.game.n_cards)
+
+    @staticmethod
+    def grid_dimensions(num_cards):
+        factors = [i for i in range(1, num_cards + 1) if num_cards % i == 0]
+        cols = min(factors, key=lambda x: abs(x - np.sqrt(num_cards)))
+        rows = int(num_cards // cols)
+        return rows, cols
+
+    @property
+    def padding(self):
+        return min(self.width, self.height) // 25
+
+    @property
+    def card_width(self):
+        return (self.width - (self.cols + 1) * self.padding) // self.cols
+    
+    @property
+    def card_height(self):
+        return (self.height - (self.rows + 2) * self.padding) // self.rows
+
+    # Draw using pygame (needs pygame.init() to be called before)
+    def render(self, state: np.ndarray) -> pygame.Surface:
+        # Constants
+        FONT_SIZE = 24
+        BG_COLOR = (30, 30, 30)
+        COLORS = (
+            (200, 200, 200), # unseen
+            (255, 0, 0), # seen, face down
+            (255, 255, 0), # face up
+            (0, 255, 0), # solved
+        )
+        TEXT_COLOR = (0, 0, 0)
+        STATE_LINE_COLOR = (255, 255, 255)
+
+        # Create display surface
+        canvas = pygame.Surface((self.width, self.height))
+
+        # Font setup
+        font = pygame.font.Font(None, FONT_SIZE)
+
+        # Fill background
+        canvas.fill(BG_COLOR)
+
+        # Draw cards
+        for card, card_state in enumerate(state):
+            row, col = divmod(card, self.cols)
+            x = col * (self.card_width + self.padding) + self.padding
+            y = row * (self.card_height + self.padding) + self.padding
+
+            color = COLORS[card_state]
+            pygame.draw.rect(canvas, color, (x, y, self.card_width, self.card_height))
+
+            if card_state != MemoryGame.CardState.UNSEEN:
+                text = self.cards[card]
+                text = f'[{self.cards[card]}] {text}'
+                text_surface = font.render(text, True, TEXT_COLOR)
+                text_rect = text_surface.get_rect(center=(x + self.card_width // 2, y + self.card_height // 2))
+                canvas.blit(text_surface, text_rect)
+
+        # Print text at the bottom
+        bottom_text = f"..."
+        bottom_text_surface = font.render(bottom_text, True, STATE_LINE_COLOR)
+        bottom_text_rect = bottom_text_surface.get_rect(center=(self.width // 2, self.height - self.padding))
+        canvas.blit(bottom_text_surface, bottom_text_rect)
+
+        return canvas
+
+    def cursor_to_card(self, x, y):
+        col = x // (self.card_width + self.padding)
+        row = y // (self.card_height + self.padding)
+        card = col + row * self.cols
+        return card
+
+    def play(self):
+        pygame.init()
+        state = self.game.reset()
+        screen = pygame.display.set_mode((self.width, self.height))
+        screen.blit(self.render(state), (0, 0))
+        pygame.display.flip()
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                action = None
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    card = self.cursor_to_card(*event.pos)
+                    # print('[RENDER] Chosen card:', card)
+                    if card < self.game.n_cards:
+                        action = card
+                if action is not None:
+                    state = self.game.step(state, action)
+                    if state is None:
+                        # print('[RENDER] Game over (Win)')
+                        running = False
+                    else:
+                        screen.blit(self.render(state), (0, 0))
+                        pygame.display.flip()
+        pygame.quit()
+
+def test_interactive_match():
+    m = InteractiveMatch(
+        MemoryGame(np.array([1, 0, 3, 2])),
+        width=800, height=600,
+        cards=('Paris', 'France', '2*(5-3)', '4'),
+        categories=('Capitals', 'Capitals', 'Math', 'Math')
+    )
+    m.play()
+
+
+
+class MemoryGameEnv(gym.Env):
+    metadata = {"render_modes": []}
+
+    def __init__(self, game: MemoryGame, max_steps=None):
+        self.game = game
+        self.max_steps = max_steps if max_steps is not None else 2**self.game.n_cards
+        self.observation_space = spaces.Discrete(1 + 4**self.game.n_cards)
+        self.action_space = spaces.Discrete(self.game.n_cards)
+        self.game_state = None
+        self.steps = None
+
+    def observe(self):
+        if self.game_state is None:
+            return 0
+        # map from array of values in 0-3 to single integer
+        return 1 + (4**np.arange(self.game.n_cards) * self.game_state).sum()
+
+    def reward(self, s, s1):
+        if s1 is None:
+            return 100.0
+        if self.game.have_new_match(s, s1):
+            # print("MATCH")
+            return 1.0
+        return -1.0
+
+    def reset(self, seed=None, options=None):
+        # print(f'[env] reset')
+        self.game_state = self.game.reset()
+        self.steps = 0
+        return self.observe(), {}
+
+    def step(self, action):
+        last_state = self.game_state
+        # print(f'[env] last_state is None: {last_state is None}')
+        self.game_state = self.game.step(self.game_state, action)
+        self.steps += 1
+        term = self.game_state is None
+        trunc = self.steps >= self.max_steps
+        # print(f'[env] state is None: {self.game_state is None}, term: {term}')
+        return self.observe(), self.reward(last_state, self.game_state), term, trunc, {}
+
+# from gymnasium.envs.registration import register
+
+# register(
+#     id="AssociativeMemoryGameEnv-v0",
+#     entry_point="upper_bound:AssociativeMemoryGameEnv",
+#     kwargs={
+#         "game": SemanticMemoryGame(),
+#     },
+#     max_episode_steps=100,
+# )
+
+class TwoAgentHandoverWrapper(gym.Wrapper):
+    """Wrapper introducing a 2-agent handover action to the environment.
+    
+    An additional action is added to the environment, which allows the current
+    agent to hand over control to a second agent. The actual handover is
+    delegated to the caller of env.step.
+    """
+
+    def __init__(self, env: gym.Env):
+        super().__init__(env)
+        assert isinstance(env.action_space, spaces.Discrete)
+        self.action_space = spaces.Discrete(env.action_space.n + 1)
+        self.observation_space = env.observation_space
+        self.last_step = None
+
+    def reset(self, **kwargs):
+        obs, info = self.env.reset(**kwargs)
+        self.last_step = obs, 0, False, False, info
+        return obs, info
+
+    def step(self, action):
+        if action == 0:
+            return self.last_step
+        self.last_step = self.env.step(action-1)
+        return self.last_step
+
+
+
+if __name__ == '__main__':
+    test_interactive_match()
--- upper_bound.py
+++ upper_bound.py
@@ -1,111 +1,154 @@
-from dataclasses import dataclass
-from typing import Callable
+from dataclasses import dataclass, field
+from functools import reduce
+from itertools import takewhile
+from typing import Callable, Iterable, Iterator, NamedTuple, TypeVar
 
+from tqdm import tqdm
+from matplotlib import pyplot as plt
 import numpy as np
+import pygame
 import gymnasium as gym
 from gymnasium import spaces
 
-from semantic_memory_game import SemanticMemoryGame
+from semantic_memory_game import MemoryGame, MemoryGameEnv
 
 
-class SemanticMemoryGameEnv(gym.Env):
-    metadata = {"render_modes": []}
+# General MDP utilities
 
-    def __init__(self, game: SemanticMemoryGame):
-        self.game = game
-        self.observation_space = spaces.Discrete(1 + 4**len(self.game.oracle))
-        self.action_space = spaces.Discrete(len(self.game.oracle))
-        self.game_state = None
+class TimeStep(NamedTuple):
+    state: object
+    action: object
+    next_state: object
+    reward: float
+    term: bool
+    trunc: bool
+    info: dict
+    next_info: dict
 
-    def observe(self):
-        if self.game_state is None:
-            return 0
-        # map from array of values in 0-3 to single integer
-        return 1 + (4**np.arange(len(self.game.oracle)) * self.game_state).sum()
+    @property
+    def done(self):
+        return self.term or self.trunc
 
-    def reward(self, s, s1):
-        if s1 is None:
-            print("WIN")
-            return 10.0
-        if self.game.have_new_match(s, s1):
-            # print("MATCH")
-            return 1.0
-        return -1.0
-
-    def reset(self, seed=None, options=None):
-        self.game_state = self.game.reset()
-        return self.observe(), {}
-
-    def step(self, action):
-        last_state = self.game_state
-        self.game_state = self.game.step(self.game_state, action)
-        term = self.game_state is None
-        trunc = False
-        return self.observe(), self.reward(last_state, self.game_state), term, trunc, {}
-
-from gymnasium.envs.registration import register
-
-register(
-    id="AssociativeMemoryGameEnv-v0",
-    entry_point="upper_bound:AssociativeMemoryGameEnv",
-    kwargs={
-        "game": SemanticMemoryGame(),
-    },
-    max_episode_steps=100,
-)
-
-
-class TwoAgentHandoverWrapper(gym.Wrapper):
-    """Wrapper introducing a 2-agent handover action to the environment.
-    
-    An additional action is added to the environment, which allows the current
-    agent to hand over control to a second agent. The actual handover is
-    delegated to the caller of env.step.
-    """
-
-    def __init__(self, env: gym.Env):
-        super().__init__(env)
-        assert isinstance(env.action_space, spaces.Discrete)
-        self.action_space = spaces.Discrete(env.action_space.n + 1)
-        self.observation_space = env.observation_space
-        self.last_step = None
-
-    def reset(self, **kwargs):
-        obs, info = self.env.reset(**kwargs)
-        self.last_step = obs, 0, False, False, info
-        return obs, info
-
-    def step(self, action):
-        if action == 0:
-            return self.last_step
-        self.last_step = self.env.step(action-1)
-        return self.last_step
-
-
-def tabular_q_learning(env: gym.Env, steps=1000, alpha=0.1, gamma=0.99, epsilon=0.1, max_episode_steps=100):
-    assert isinstance(env.observation_space, spaces.Discrete)
-    assert isinstance(env.action_space, spaces.Discrete)
-    q = np.zeros((env.observation_space.n, env.action_space.n))
+def steps(env: gym.Env, policy: Callable[[object], object]):
     term = True
     trunc = False
-    episode_steps = 0
-    for _ in range(steps):
-        if term or trunc or episode_steps >= max_episode_steps:
-            state = env.reset()
-        if np.random.rand() < epsilon:
-            action = env.action_space.sample()
-        else:
-            action = np.argmax(q[state])
-        next_state, reward, term, trunc, _ = env.step(action)
-        q[state, action] += alpha * (reward + term * gamma * np.max(q[next_state]) - q[state, action])
+    while True:
+        if term or trunc:
+            state, info = env.reset()
+        action = policy(state)
+        next_state, reward, term, trunc, next_info = env.step(action)
+        yield TimeStep(state, action, next_state, reward, term, trunc, info, next_info)
         state = next_state
-        episode_steps += 1
-    return q
 
+def episode(env: gym.Env, policy: Callable[[object], object]):
+    return takewhile(lambda ts: not ts.done, steps(env, policy))
+
+def eval_policy(env: gym.Env, policy: Callable[[object], object]):
+    return reduce(lambda acc, ts: (acc[0] + ts.reward, acc[0]+1), episode(env, policy), (0, 0))
+
+def mean_eval(env: gym.Env, policy: Callable[[object], object], n=10):
+    evals = np.array([eval_policy(env, policy) for _ in range(n)])
+    return evals[:,0].mean(), evals[:,1].mean()
+
+T = TypeVar('T')
+
+@dataclass(frozen=True)
+class Take(Iterable[T]):
+    n: int
+    iterable: Iterable[T]
+
+    def __len__(self):
+        return self.n
+
+    def __iter__(self) -> Iterator[T]:
+        for i, x in enumerate(self.iterable):
+            if i >= self.n:
+                break
+            yield x
+
+
+# Tabular Q-Learning
+
+@dataclass
+class TabularQLearning:
+    alpha: float = 0.1
+    gamma: float = 0.9
+    epsilon_schedule: Callable[[int], float] = lambda i: 0.1
+
+    @staticmethod
+    def decay_schedule(initial: float= 0.1, decay: float = 0.999):
+        return lambda i: initial * decay**i
+
+    @staticmethod
+    def linear_schedule(initial: float = 0.1, final: float = 0.01, n: int = 10000):
+        return lambda i: max(final, initial - i * (initial - final) / n)
+
+    @dataclass
+    class State:
+        q: np.ndarray
+        steps: int
+        algo: 'TabularQLearning'
+
+        @property
+        def epsilon(self):
+            return self.algo.epsilon_schedule(self.steps)
+
+        def best_action(self, s):
+            return np.argmax(self.q[s])
+
+        def epsilon_greedy_action(self, s):
+            if np.random.rand() < self.epsilon:
+                return np.random.choice(self.q.shape[1])
+            return self.best_action(s)
+
+    def update(self, q: np.ndarray, ts: TimeStep):
+        q[ts.state, ts.action] += self.alpha * (ts.reward + ts.term * self.gamma * np.max(q[ts.next_state]) - q[ts.state, ts.action])
+
+    def run(self, env: gym.Env, q: np.ndarray | None = None) -> Iterator[State]:
+        assert isinstance(env.observation_space, spaces.Discrete)
+        assert isinstance(env.action_space, spaces.Discrete)
+        if q is None:
+            q = np.zeros((env.observation_space.n, env.action_space.n))
+        algo_state = TabularQLearning.State(q, 0, self)
+        for i, ts in enumerate(steps(env, algo_state.epsilon_greedy_action)):
+            self.update(algo_state.q, ts)
+            algo_state.steps = i
+            yield TabularQLearning.State(algo_state.q.copy(), i, self)
 
 
 def main():
-    pass
+    def make_env():
+        return MemoryGameEnv(MemoryGame(oracle=np.array([1, 0, 3, 2])))
+    env = make_env()
+    eval_env = make_env()
+
+    n_steps = 500_000
+    epsilon_schedule = TabularQLearning.linear_schedule(initial=0.5, n=n_steps)
+    algo = TabularQLearning(epsilon_schedule=epsilon_schedule)
+
+    eval_returns = []
+    eval_lens = []
+    epsilons = []
+    for i, agent in enumerate(tqdm(Take(n_steps, algo.run(env)))):
+        # print(f'i: {i}')
+        if i % 1000 == 0:
+            # evals.append(eval_policy(eval_env, agent.best_action))
+            ret, len = mean_eval(eval_env, agent.best_action)
+            eval_returns.append(ret)
+            eval_lens.append(len)
+            epsilons.append(agent.epsilon)
+    print(f'learned Q: {agent.q.shape}')
+
+    plt.plot(eval_returns, label='eval returns')
+    plt.show()
+    # plt.legend()
+
+    plt.plot(eval_lens, label='eval episode lengths')
+    plt.show()
+
+    plt.plot(epsilons, label='epsilon')
+    plt.show()
+
 
 
 if __name__ == "__main__":
